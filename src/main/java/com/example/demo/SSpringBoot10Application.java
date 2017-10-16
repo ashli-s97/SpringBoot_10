2b7cb4f3a9ceeb399a4b633c9e7b68f08e785bb7
ashli-s97 <31264613+ashli-s97@users.noreply.github.com>
2017-10-16 16:05:16 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/SSpringBoot10Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SSpringBoot10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SSpringBoot10Application.class, args);
+	}
+}
